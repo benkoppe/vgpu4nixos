0f1e4af9a5e6fd2205ca72a2e181ecdb4b34cc78
ben <koppe.development@gmail.com>
2026-07-12 22:31:30 -0700

    Replace lib.fakeHash with correct hash

--- default.nix
+++ default.nix
@@ -96,8 +96,7 @@ let
         version = "570.124";
         rev = "2049bd772afed18befa58a2f8a98e5aca3f8b27c";
 
-        # Replace this after the first build reports the correct hash.
-        sha256 = lib.fakeHash;
+        sha256 = "sha256-qW7YVv+TE6ytn+rkXEwLcV6TJsdbuZyiXXBfcwrfCRI=";
 
         generalVersion = "570.124.04";
 
